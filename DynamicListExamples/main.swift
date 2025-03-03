0a193276d83a03d7c2dfe9f1fdcc5ad1787fd8a0
Josephine Salamon <jsalamon@lcs.on.ca>
2025-03-03 08:54:10 -0500

    inputed the code from the lesson

--- DynamicListExamples/main.swift
+++ DynamicListExamples/main.swift
@@ -7,5 +7,51 @@
 
 import Foundation
 
-print("Hello, World!")
+// Create an array of integers that
+// holds three random numbers to start
+var numbers: [Int] = [
+ 
+    Int.random(in: 1...100),
+    Int.random(in: 1...100),
+    Int.random(in: 1...100),
+ 
+]
+ 
+// Print the values in the array
+for number in numbers {
+    print(number)
+}
 
+// Add two more random numbers to the array
+numbers.append(Int.random(in: 1...100))
+numbers.append(Int.random(in: 1...100))
+ 
+// Print a divider
+print("----------")
+ 
+// Print the values in the array again
+for number in numbers {
+    print(number)
+}
+
+// Print a divider
+print("----------")
+ 
+// Ask the user how many more numbers to add to the array
+print("How many more elements would you like to add to the array? ", terminator: "")
+let additionalElementCount = Int(readLine()!)!
+ 
+// Prompt the user to add more elements
+for _ in 0..<additionalElementCount {
+    print("Please enter a number: ", terminator: "")
+    let newValue = Int(readLine()!)!
+    numbers.append(newValue)
+}
+ 
+// Print a divider
+print("----------")
+ 
+// Print the values in the array a third time
+for number in numbers {
+    print(number)
+}
